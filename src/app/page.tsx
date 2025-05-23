c79cc94debca3c6216d528b4ad6e0bb00f9527c7
Anil Vishwakarma <anil.vishwakarma@watchyourhealth.com>
2025-05-23 18:33:14 +0000

    can you add my login page to here?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  redirect('/dashboard');
+  redirect('/login');
   // The redirect function throws an error to stop rendering, so no need to return null.
 }
